1514c8cc802838c8d3037be2d3bf38f437e449c4
Aayush Patidar <aayushp717@gmail.com>
2024-08-21 16:10:51 +0530

    aayush first push

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='bg-black'>Home</div>
+    <div className='bg-black text-white'>Home</div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -4,5 +4,6 @@ const app=express();
 
 
 app.listen(3000,()=>{
-    console.log('server is running on port 3000!!')
+    console.log('server is running on port 3000!! lund')
 })  
+
